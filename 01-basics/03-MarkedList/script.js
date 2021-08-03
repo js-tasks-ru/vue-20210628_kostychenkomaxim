7449a4decc2cb2d5c5c6626287fd99370fccc790
7nn <kostychenkomaxim@gmail.com>
2021-08-03 15:37:21 +0300

    marked list

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -30,3 +30,20 @@ const emails = [
 ];
 
 // Требуется создать Vue приложение
+
+const app = createApp({
+  data() {
+    return {
+      emailsList: emails,
+      searchingEmail: '',
+    };
+  },
+  computed: {
+    filteredEmailsList() {
+      return this.emailsList.map((email) => ({
+        email,
+        marked: this.searchingEmail && email.includes(this.searchingEmail),
+      }));
+    },
+  },
+}).mount('#app');
